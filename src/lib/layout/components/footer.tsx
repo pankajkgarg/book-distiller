61e5066a395e9bc291b9bd61b72f2608351f4df0
Pankaj Garg <pankajn17@gmail.com>
2025-08-10 23:58:18 +0200

    cleanup

--- src/lib/layout/components/footer.tsx
+++ src/lib/layout/components/footer.tsx
@@ -1,18 +1,3 @@
 export const Footer = () => {
-  return (
-    <footer className="wrapper">
-      <div className="flex">
-        <p className="text-xs">
-          {new Date().getFullYear()} -{' '}
-          <a
-            href="https://agustinusnathaniel.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            agustinusnathaniel.com
-          </a>
-        </p>
-      </div>
-    </footer>
-  );
+  return null;
 };
